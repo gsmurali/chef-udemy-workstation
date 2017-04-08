5fc2cc4c41a04becd97d1aeffdcf8651ebc09761
root <root@humshini1.mylabserver.com>
2017-04-08 03:07:05 +0000

    initial workstation commit

---
+++ recipes/setup.rb
@@ -0,0 +1,19 @@
+package 'tree' do
+ action :install
+end
+
+package 'git' do
+ action :install
+end 
+
+package 'ntp' do
+ action :install
+end
+
+file '/etc/motd' do
+  content 'This server is the property of Murali'
+  owner 'root'
+  group 'root'
+end
+
+
